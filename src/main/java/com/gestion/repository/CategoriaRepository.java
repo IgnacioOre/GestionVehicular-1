25ec3a28a7da5d970e142852e3a3bb69810f415c
elCharmandersito <73306782+elCharmandersito@users.noreply.github.com>
2020-12-09 15:24:40 -0300

    Implementación Ultima Test CategoriaServiceTest

--- src/main/java/com/gestion/repository/CategoriaRepository.java
+++ src/main/java/com/gestion/repository/CategoriaRepository.java
@@ -1,13 +1,8 @@
 package com.gestion.repository;
 
-import java.util.List;
-
-import org.springframework.dao.DataAccessException;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.gestion.model.Categoria;
-import com.gestion.model.Producto;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
 	
