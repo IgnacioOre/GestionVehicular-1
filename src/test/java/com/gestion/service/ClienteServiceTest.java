dc82040807ef6a9932bb2764f7a78b9a2215f4bf
Volt4 <43504070+Volt4@users.noreply.github.com>
2020-12-09 00:06:01 -0300

    Update ClienteServiceTest.java

--- src/test/java/com/gestion/service/ClienteServiceTest.java
+++ src/test/java/com/gestion/service/ClienteServiceTest.java
@@ -3,6 +3,7 @@ package com.gestion.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
@@ -12,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.gestion.model.Cliente;
@@ -25,12 +27,12 @@ class ClienteServiceTest {
 	
 	
 	@Mock
-	private ClienteRepositorylmpl clienteRepositorylmpl;
+	private ClienteRepositorylmpl clienteRepositoryImpl;
 	
 	
 
 	@InjectMocks
-	private ClienteServiceImpl clienteServicelmpl;
+	private ClienteServiceImpl clienteServiceImpl;
 	
 	@Test
 	void siSeInvocaGetAllClientesDebeRetornarListaCliente() {
@@ -40,10 +42,10 @@ class ClienteServiceTest {
 		cliente.add(clientes);
 		cliente.add(clientedos);
 		
-		when(clienteRepositorylmpl.findAll()).thenReturn(cliente);
+		when(clienteRepositoryImpl.findAll()).thenReturn(cliente);
 		
 		List<Cliente> resultado;
-		resultado= clienteServicelmpl.getAllClientes();
+		resultado= clienteServiceImpl.getAllClientes();
 		
 		assertEquals(cliente.size(),resultado.size());
 		assertThat(cliente.get(0).getRut()).isEqualTo("111-1");
@@ -52,19 +54,29 @@ class ClienteServiceTest {
 	@Test
 	void siSeInvocaGetAllClientesYNoExistenClientesDevuelveUnaListaVacia() {
 		List <Cliente> cliente = new ArrayList<Cliente>();
-		when(clienteRepositorylmpl.findAll()).thenReturn(cliente);
+		when(clienteRepositoryImpl.findAll()).thenReturn(cliente);
 		
 		List <Cliente> resultado;
-		resultado= clienteServicelmpl.getAllClientes();
+		resultado= clienteServiceImpl.getAllClientes();
 		
 		assertEquals(0,resultado.size());
 				
 	}
 	
-	/* @Test
-	 void siSeInvocaSaveYNoSePuedeGuardarLanzarException() throws Exception {
+	 @Test
+	 void siSeInvocaSaveYSeGuardaCliente() throws Exception {
+		List <Cliente> cliente = new ArrayList<>();
+		Cliente clientes = new Cliente("111-3","Daniel","Lopez","Lopez","San Carlos","emailDan@mail");
+		cliente.add(clientes);
+		clienteServiceImpl.save(clientes);
+		Mockito.verify(clienteRepositoryImpl,times(1)).save(clientes);
 		
-	 }*/
+	 }
+	 
+	 @Test
+	 void siSeInvocaSaveYSeLanzaException() throws Exception{
+			assertThrows(Exception.class,()->clienteServiceImpl.save(null));
+	 }
 	
 	
 
