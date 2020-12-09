b8f813a3d1cc0ed95242e9231771e45ab30dd58e
AlejandraOC <75698891+AlejandraOC@users.noreply.github.com>
2020-12-08 22:03:56 -0300

    Create ProductoRestControllerTest.java

---
+++ src/test/java/com/gestion/rest/ProductoRestControllerTest.java
@@ -0,0 +1,14 @@
+package com.gestion.rest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProductoRestControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
