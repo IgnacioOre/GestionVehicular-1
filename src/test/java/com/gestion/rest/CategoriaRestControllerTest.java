3ca1b7da068998f18a40ce12da42cdfc06a4b22f
elCharmandersito <73306782+elCharmandersito@users.noreply.github.com>
2020-12-09 21:20:03 -0300

    Update CategoriaRestControllerTest.java

--- src/test/java/com/gestion/rest/CategoriaRestControllerTest.java
+++ src/test/java/com/gestion/rest/CategoriaRestControllerTest.java
@@ -1,9 +1,7 @@
 package com.gestion.rest;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.doThrow;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import java.util.ArrayList;
@@ -18,7 +16,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.json.JacksonTester;
-import org.springframework.dao.DataRetrievalFailureException;
 import org.springframework.http.HttpStatus;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
@@ -26,85 +23,86 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gestion.model.Categoria;
-import com.gestion.model.Producto;
 import com.gestion.service.CategoriaServiceImpl;
 
 @ExtendWith(MockitoExtension.class)
 class CategoriaRestControllerTest {
-	
+
 	private MockMvc mockMvc;
-	
+
 	@Mock
 	private CategoriaServiceImpl categoriaServiceImpl;
-	
+
 	@InjectMocks
 	private CategoriaRestController categoriaRestController;
-	
+
 	private JacksonTester<List<Categoria>> jsonListCategoria;
-	
+
 	@BeforeEach
 	void setup() {
 		JacksonTester.initFields(this, new ObjectMapper());
 		mockMvc = MockMvcBuilders.standaloneSetup(categoriaRestController).build();
 	}
 
-	//@Test
+	// @Test
 	void siSeInvocaGetAllCategoriasYExistenDebeRetornarUnaLIstaYOk() throws Exception {
 		// Given
 		ArrayList<Categoria> categorias = new ArrayList<Categoria>();
 		categorias.add(new Categoria(1, "Llantas"));
 		categorias.add(new Categoria(2, "Bujias"));
-		
+
 		given(categoriaServiceImpl.getAllCategorias()).willReturn(categorias);
-		
+
 		// When
-		
-		MockHttpServletResponse response = mockMvc.perform(get("/categorias")
-	            .accept(MediaType.APPLICATION_JSON))
-	            .andReturn()
-	            .getResponse();  
-		
+
+		MockHttpServletResponse response = mockMvc.perform(get("/categorias").accept(MediaType.APPLICATION_JSON))
+				.andReturn().getResponse();
+
 		// Then
 		assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
-		assertThat(response.getContentAsString()).isEqualTo(
-				jsonListCategoria.write(categorias).getJson());
+		assertThat(response.getContentAsString()).isEqualTo(jsonListCategoria.write(categorias).getJson());
 	}
-	
-	//@Test
+
+	// @Test
 	void siSeInvocaGetAllCategoriasYNoExistenDebeRetornarNotFound() throws Exception {
 		// Give
-		ArrayList<Categoria> categorias= new ArrayList<Categoria>();
+		ArrayList<Categoria> categorias = new ArrayList<Categoria>();
 		given(categoriaServiceImpl.getAllCategorias()).willReturn(categorias);
-		
+
 		// When
-		MockHttpServletResponse response = mockMvc.perform(get("/categorias")
-				.accept(MediaType.APPLICATION_JSON))
-				.andReturn()
-				.getResponse();
-		
+		MockHttpServletResponse response = mockMvc.perform(get("/categorias").accept(MediaType.APPLICATION_JSON))
+				.andReturn().getResponse();
+
 		// Then
 		assertThat(response.getStatus()).isEqualTo(HttpStatus.NOT_FOUND.value());
 	}
-	
-	@Test
-	void siSeInvocaAddCategoriaConUnaCategoriaValidaDebeAlmacenarlaYRetornarCreated() {
-		// Give
-		Categoria categoria = new Categoria(2, "cosas pedro");
 
-		//given(categoriaServiceImpl.save(categoria)).willReturn(categoria);
-		
+	@Test
+	void siSeInvocaAddCategoriaConUnaCategoriaValidaDebeAlmacenarlaYRetornarCreated() throws Exception {
+		// Give
+		//Categoria categoria = new Categoria(2, "cosas pedro");
+
+		//doThrow().when(categoriaServiceImpl.save(categoria)).thenReturn(categoria);
+
+
 		// When
-		//MockHttpServletResponse response = mockMvc.perform(get("/categorias/agregar")
-		//		.accept(MediaType.APPLICATION_JSON))
-		//		.andReturn()
-		//		.getResponse();
-		
+		MockHttpServletResponse response = mockMvc.perform(get("/categorias/agregar")
+		 .accept(MediaType.APPLICATION_JSON))
+		 .andReturn()
+		 .getResponse();
+
 		// Then
-		//assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());	    
+		 assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());
 	}
-	
+
 	//@Test
-	void siSeInvocaAddCategoriaConParametroInvalidoDebeRetornarBadRequest() {
-		fail("Not yet implemented");
+	void siSeInvocaAddCategoriaConParametroInvalidoDebeRetornarBadRequest() throws Exception {
+		// Give			
+		
+
+		// When
+		
+
+		// Then		
 	}
 }
