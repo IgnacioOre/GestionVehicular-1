9046417442fae98150f48c1586c1de129316b749
elCharmandersito <73306782+elCharmandersito@users.noreply.github.com>
2020-12-09 23:32:34 -0300

    Update - LineaListaDeDeseoRestControllerTest
    Se implemento la clase con los test correspondientes

--- src/test/java/com/gestion/rest/LineaListaDeDeseosRestControllerTest.java
+++ src/test/java/com/gestion/rest/LineaListaDeDeseosRestControllerTest.java
@@ -1,9 +1,13 @@
 package com.gestion.rest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.ws.rs.core.MediaType;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,14 +16,17 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gestion.model.Cliente;
 import com.gestion.model.LineaListaDeDeseos;
+import com.gestion.model.ListaDeDeseo;
 import com.gestion.model.Producto;
 import com.gestion.service.LineaListaDeDeseosServiceImpl;
-import com.gestion.service.ProductoServiceImpl;
 
 @ExtendWith(MockitoExtension.class)
 class LineaListaDeDeseosRestControllerTest {
@@ -32,6 +39,8 @@ class LineaListaDeDeseosRestControllerTest {
 	@InjectMocks
 	private LineaListaDeDeseosRestController lineaListaDeDeseosRestCotroller;
 	
+	private JacksonTester<List<LineaListaDeDeseos>> jsonLineaListaDeDeseos;
+	
 	@BeforeEach
 	void setup() {
 		JacksonTester.initFields(this, new ObjectMapper());
@@ -39,16 +48,53 @@ class LineaListaDeDeseosRestControllerTest {
 	}
 	
 	@Test
-	void siSeInvocaGetAllLineaListaDeDeseosYSeAgregaCorrectamenteRetornaLaListaYOK() {
-		//given
-		ArrayList <LineaListaDeDeseos> listaDeDeseo= new ArrayList<LineaListaDeDeseos>();
+	void siSeInvocaGetAllLineaListaDeDeseosYSeAgregaCorrectamenteRetornaLaListaYOK() throws Exception {
+		//given		
+		Cliente cliente = new Cliente("123456789", "Alejandra", "Ortiz", "Cea", "suCasa", "suCorreo@gmail.com");
 		
+		ListaDeDeseo listaDeseo = new ListaDeDeseo(1, "cosas cliente", cliente);		
 		
+		ArrayList<LineaListaDeDeseos> linea = new ArrayList<LineaListaDeDeseos>();
 		
+		ArrayList<Producto> productos = new ArrayList<Producto>();
+		Producto a = new Producto(1, "nombreA", "marcaA", "modeloA", 12000, 12);
+		Producto b = new Producto(2, "nombreB", "marcaB", "modeloB", 22000, 13);
+		
+		productos.add(a);
+		productos.add(b);
+		
+		LineaListaDeDeseos linea1 = new LineaListaDeDeseos(1, productos, listaDeseo, "cosas varias");
+		
+		linea.add(linea1);
+		
+		given(lineaListaDeseosService.getAllList()).willReturn(linea);		
 		
 		//when
+		MockHttpServletResponse response = mockMvc.perform(get("/linea_lista_de_deseos")
+	            .accept(MediaType.APPLICATION_JSON))
+	            .andReturn()
+	            .getResponse();  
 		
 		//Then
+		assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+		assertThat(response.getContentAsString()).isEqualTo(
+				jsonLineaListaDeDeseos.write(linea).getJson());
 		
+	}
+	
+	@Test
+	void siSeInvocaGetAllLineaListaDeDeseosYNoExistenRetornaNotFound() throws Exception {
+		// Given
+		ArrayList<LineaListaDeDeseos> linea = new ArrayList<LineaListaDeDeseos>();
+		given(lineaListaDeseosService.getAllList()).willReturn(linea);
+		
+		// When
+		MockHttpServletResponse response = mockMvc.perform(get("/linea_lista_de_deseos")
+	            .accept(MediaType.APPLICATION_JSON))
+	            .andReturn()
+	            .getResponse();  
+		
+		// Then
+		assertThat(response.getStatus()).isEqualTo(HttpStatus.NOT_FOUND.value());			
 	}
 }
